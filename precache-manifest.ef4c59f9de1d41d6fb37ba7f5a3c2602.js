69f4230db30ae90ab00b4df0834a68f96aa46be6
paula <paulagrabiela.anez@students.salle.url.edu>
2019-08-25 22:32:56 +0200

    Updates

---
+++ precache-manifest.ef4c59f9de1d41d6fb37ba7f5a3c2602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5fee02e6772009019b171cb639f7a1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f193ead2b4cccae649bf",
+    "url": "/monsters-rolodex/static/css/main.4eed3e26.chunk.css"
+  },
+  {
+    "revision": "607e395712115653f754",
+    "url": "/monsters-rolodex/static/js/2.45dda9c7.chunk.js"
+  },
+  {
+    "revision": "f193ead2b4cccae649bf",
+    "url": "/monsters-rolodex/static/js/main.95528bc0.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
